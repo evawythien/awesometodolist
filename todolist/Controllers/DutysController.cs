04d33be658e65d78ce65a7c63995dcd0e3034844
Eva <evadvgarcia@gmail.com>
2021-05-24 21:48:07 +0200

    Add controller

---
+++ todolist/Controllers/DutysController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using todolist.Entities;
+using todolist.Services;
+
+namespace todolist.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DutysController : ControllerBase
+    {
+        /// <summary>
+        /// Duty service.
+        /// </summary>
+        private readonly DutyServices services;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="services"></param>
+        public DutysController(DutyServices services)
+        {
+            this.services = services;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Duty> dutys = this.services.Get();
+            return Ok(dutys);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Duty newDuty)
+        {
+            this.services.Create(newDuty);
+            return Ok(new { id = newDuty.Id });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, Duty bookIn)
+        {
+            this.services.Update(id, bookIn);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            this.services.Remove(id);
+            return NoContent();
+        }
+    }
+}
