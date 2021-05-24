88658ff3ad5eb1be5c58ab2d211e3c247242ed53
Eva <evadvgarcia@gmail.com>
2021-05-24 21:41:31 +0200

    DB Config

---
+++ todolist/Core/DBOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace todolist.Core
+{
+    public class DBOptions
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
