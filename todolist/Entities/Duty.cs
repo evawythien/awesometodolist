43afa08f04068a13e17586bec04c44bf2c507a9d
Eva <evadvgarcia@gmail.com>
2021-05-24 21:44:51 +0200

    Add entity

---
+++ todolist/Entities/Duty.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace todolist.Entities
+{
+    public class Duty
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool Completed { get; set; }
+    }
+}
